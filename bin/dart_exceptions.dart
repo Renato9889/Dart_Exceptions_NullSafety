ab7f7cd048ae7532ed0d25ba26712b7820e9cf86
Renato Luiz <renatoluiz9834@hotmail.com>
2023-02-18 19:23:22 -0300

    Projeto Exceptions

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -2,25 +2,50 @@
 import 'controllers/bank_controller.dart';
 import 'models/account.dart';
 
-void main() {
+/* void main() {
   // Criando o banco
   BankController bankController = BankController();
 
   // Adicionando contas
   bankController.addAccount(
-      id: "Ricarth",
+      id: "Paulo",
       account:
-          Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+          Account(name: "Paulo Filho", balance: 400, isAuthenticated: true));
 
   bankController.addAccount(
-      id: "Kako",
+      id: "Renato",
       account:
-          Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+          Account(name: "Renato Luiz", balance: 600, isAuthenticated: true));
 
   // Fazendo transferência
   bool result = bankController.makeTransfer(
-      idSender: "Kako", idReceiver: "Ricarth", amount: 700);
+      idSender: "Renato", idReceiver: "Paulo", amount: 200);
 
   // Observando resultado
   print(result);
+}
+ */
+
+void main() {
+  int i = 0;
+  print("Started Main");
+  functionOne();
+  print("Finished Main");
+}
+
+functionOne() {
+  int j = 25;
+  print("Started F01");
+  functionTwo(j);
+  print("Finished F01");
+}
+
+functionTwo(int otherJ) {
+  int k = 0;
+  print("Started F02");
+  for (int i = 1; i <= 5; i++) {
+    print(i);
+  }
+  otherJ = 50;
+  print("Finished F02");
 }
